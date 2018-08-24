4842bbedb7dd67b0e90431154911840f63eee947
Clement MASSARD <cmassard@palo-it.com>
2018-08-24 15:18:07 +0200

    Create component and set up client-side routing with react router

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import AppRouter from './routers/AppRouter';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<p>Expensify App</p>, document.getElementById('app'));
+ReactDOM.render(<AppRouter />, document.getElementById('app'));
